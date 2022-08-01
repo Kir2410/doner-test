1e299d2f5de83fdc9d954fb864c2b439e5334a21
Кирилл Коршунов <kkv2410@gmail.com>
2022-08-01 23:45:22 +0700

    Create promise in Catalog.js

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -3,14 +3,40 @@ import Doner from "./Doner";
 import styles from "../css/Catalog.module.css";
 
 class Catalog extends React.Component {
+  state = { doners: [] };
+
+  componentDidMount() {
+    fetch("https://api.sampleapis.com/coffee/hot")
+      .then((resp) => resp.json())
+      .then((data) => this.setState({ doners: data }));
+  }
+
   render() {
     return (
       <div className={styles.catalog}>
         <Doner
           image={
-            "https://papalavash.ru/wp-content/uploads/2020/11/doner.jpg-2.jpg"
+            "https://stoneforest.ru/wp-content/uploads/2021/11/dalnevostochnyi-fast-fud-shaurma-ne-tak-prosta-kak-kazhetsya-1.jpg"
           }
           name={"Шаурма Классическая маленькая, 300 гр."}
+        />
+        <Doner
+          image={
+            "https://stoneforest.ru/wp-content/uploads/2021/11/dalnevostochnyi-fast-fud-shaurma-ne-tak-prosta-kak-kazhetsya-1.jpg"
+          }
+          name={"Шаурма Классическая средняя, 450 гр."}
+        />
+        <Doner
+          image={
+            "https://stoneforest.ru/wp-content/uploads/2021/11/dalnevostochnyi-fast-fud-shaurma-ne-tak-prosta-kak-kazhetsya-1.jpg"
+          }
+          name={"Шаурма Классическая большая, 600 гр."}
+        />
+        <Doner
+          image={
+            "https://stoneforest.ru/wp-content/uploads/2021/11/dalnevostochnyi-fast-fud-shaurma-ne-tak-prosta-kak-kazhetsya-1.jpg"
+          }
+          name={"Шаурма Классическая огромная, 800 гр."}
         />
       </div>
     );
